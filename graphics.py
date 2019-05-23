647ed38775380af1ad03a01170e5de9995103998
Caleb <calebwhitaker24@gmail.com>
2019-05-23 14:57:04 -0500

    Added my image processor to github

---
+++ graphics.py
@@ -0,0 +1,390 @@
+import pygame
+pygame.init()
+
+size = (600, 600)
+
+screen = pygame.display.set_mode(size)
+
+image_file = pygame.image.load("images/saved.JPG")
+
+image = pygame.transform.scale(image_file, (600,600))
+
+def brighten():
+    for x in range(0, 600):
+        for y in range(0, 600):
+            color = screen.get_at((x, y))
+            r = color[0] + 15
+            g = color[1] + 15
+            b = color[2] + 15
+
+            if r > 255:
+                r = 255
+            if g > 255:
+                g = 255
+            if b > 255:
+                b = 255
+
+            screen.set_at((x, y), (r, g, b))
+
+def darken():
+    for x in range(0, 600):
+        for y in range(0, 600):
+            color = screen.get_at((x, y))
+            r = color[0] - 15
+            g = color[1] - 15
+            b = color[2] - 15
+
+            if r < 0:
+                r = 0
+            if g < 0:
+                g = 0
+            if b < 0:
+                b = 0
+
+            screen.set_at((x, y), (r, g, b))
+
+def removeRed():
+    for x in range(0,600):
+        for y in range(0,600):
+            color = screen.get_at((x, y)) # color = [r,g,b,a]
+
+            r = 0
+            g = color[1]
+            b = color[2]
+
+            screen.set_at((x, y), (r, g, b))
+
+def removeGreen():
+    for x in range(0,600):
+        for y in range(0,600):
+            color = screen.get_at((x, y)) # color = [r,g,b,a]
+
+            r = color[0]
+            g = 0
+            b = color[2]
+
+            screen.set_at((x, y), (r, g, b))
+
+def removeBlue():
+    for x in range(0,600):
+        for y in range(0,600):
+            color = screen.get_at((x, y)) # color = [r,g,b,a]
+
+            r = color[0]
+            g = color[1]
+            b = 0
+
+            screen.set_at((x, y), (r, g, b))
+
+def negate():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+
+            r = 255 - color[0] # r = 100, new r = 155
+            g = 255 - color[1] # g = 55, new g = 200
+            b = 255 - color[2]
+
+            screen.set_at((x, y), (r, g, b))
+
+def intensify():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x,y))
+
+            r = color[0]
+            g = color[1]
+            b = color[2]
+
+            if r > 255/2:
+                r = 255
+            else:
+                r = 0
+
+            if g > 255/2:
+                g = 255
+            else:
+                g = 0
+
+            if b > 255/2:
+                b = 255
+            else:
+                b = 0
+
+            screen.set_at((x, y), (r, g, b))
+
+def grayscale():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x,y))
+
+            r = color[0]
+            g = color[1]
+            b = color[2]
+
+            average = (r+g+b)/3
+
+            screen.set_at((x,y), (average, average, average))
+
+def twoTone():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x,y))
+
+            r = color[0]
+            g = color[1]
+            b = color[2]
+
+            # New work:
+
+            brightness = r + g + b
+
+            if brightness > (255*3)/2.0:
+               screen.set_at((x,y), (255, 255, 255))
+
+            else:
+                screen.set_at((x,y), (0, 0, 0))
+
+def fourTone():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+
+            max = 255*3
+
+            r = color[0]
+            g = color[1]
+            b = color[2]
+
+            brightness = r + g + b
+
+            if brightness > (3/4) * max:
+                screen.set_at((x,y), (240, 240, 120))
+
+            elif brightness > (2/4) * max:
+                screen.set_at((x,y), (240, 30, 30))
+
+            elif brightness > (1/4) * max:
+                screen.set_at((x,y), (15, 150, 15))
+
+            else:
+                screen.set_at((x,y), (15, 15, 80))
+
+def maxRed():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+
+            r = 255
+            g = color[1]
+            b = color[2]
+
+            screen.set_at((x, y), (r, g, b))
+
+def maxBlue():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+            r = color[0]
+            g = color[1]
+            b = 255
+
+            screen.set_at((x, y), (r, g, b))
+
+
+def maxGreen():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+            r = color[0]
+            g = 255
+            b = color[2]
+
+            screen.set_at((x, y), (r, g, b))
+
+def maxPurple():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+            r = 255
+            g = color[1]
+            b = 255
+
+            screen.set_at((x, y), (r, g, b))
+
+def maxYellow():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+            r = 255
+            g = 255
+            b = color[2]
+
+            screen.set_at((x, y), (r, g, b))
+
+
+def maxCyan():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+            r = color[0]
+            g = 255
+            b = 255
+
+            screen.set_at((x, y), (r, g, b))
+
+
+def special():
+    negate() # 360000
+    brighten() # 360000
+    removeRed() # 360000
+    removeGreen()
+
+def verticleStripes():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+
+            if x % 2 == 0:
+
+                r = color[0]
+                g = color[1]
+                b = color[2]
+                screen.set_at((x, y), (r, g, b))
+
+            else:
+                screen.set_at((x, y), (0, 255, 0))
+
+
+
+def horizontalStripes():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+
+            if y % 2 == 0:
+
+                r = color[0]
+                g = color[1]
+                b = color[2]
+                screen.set_at((x, y), (r, g, b))
+
+            else:
+                screen.set_at((x, y), (0, 255, 0))
+
+
+def sepia():
+    for x in range(600):
+        for y in range(600):
+            color = screen.get_at((x, y))
+
+            r = color[0]
+            g = color[1]
+            b = color[2]
+
+
+
+
+
+            final_r = .393 * r + .769 * g + .189 * b
+            final_g = .349 * r + .686 * g + .168 * b
+            final_b = .272 * r + .534 * g + .131 * b
+
+            final_r = int(final_r)
+            final_g = int(final_g)
+            final_b = int(final_b)
+
+            screen.set_at((x, y), (final_r, final_g, final_b))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_l:
+                screen.blit(image, image.get_rect())
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                screen.fill([0,255,0])
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                pygame.draw.circle(screen, [255, 0, 0], (300,300), 10)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_b:
+                brighten()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                darken()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_2:
+                removeRed()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_3:
+                removeGreen()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_4:
+                removeBlue()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                pygame.image.save(screen, "saved.jpg")
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_5:
+                negate()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_6:
+                intensify()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_g:
+                grayscale()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_t:
+                twoTone()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_f:
+                fourTone()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                maxRed()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                special()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                maxBlue()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                maxGreen()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                maxPurple()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_y:
+                maxYellow()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_o:
+                maxCyan()
+
+
+
+
+    pygame.display.flip()
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Image Processing Project
+
+This project is the final project of the Computer Science 3 - it involves 
+using Python and PyGame to manipulate 2D Graphics
+
+|Key| Function | Description| 
+|---|----------|------------|
+| r | maxRed() | Will make the image as red as possible|
+| l | load Image | Displays the image                  |
+| b | brighten()| Brightens image                      |
+| 1 | darken()  | Darken image                         |
+| 2 | removeRed()| Removes red in images                        |
+| 3 | removeGreen()| Removes green                    |
+| 4 | removeBlue() | Remove blue                       |
+| 5 | pygame.image.save(screen, "saved.jpg")()| Saves Image|
+| 6 | negate()| Flips the red, green, and blue values|
+| g | grayscale()| Makes the image gray|
+| t | twoTone()| Makes the image consist of only black and white|
+| f | fourTone()| Makes the image of only red, green, blue and yellow|
+| 7 | sepia()  | Makes the image appear sepia|
+| 8 | harryPotter6()|Makes the image appear like Harry Potter|
+| 9 | purpleAura()|Makes the image taking on a purplish haze|
+|10 | verticalStripes()|Every other column in the image is completely green|
+|11 | horizontalStripes()| Every other row in the image is completely green|     
+|12 | mirrorXAxis()| Flips the image over the "X-Axis"|
+|13 | mirrorYAxis()| Flips the image over the "Y-Axis"|
+|14 | fadeFromTop()| At the top of the image, the percentage of the image that is unchanged is 0%. At the bottom, every pixel is unchanged|
+|15 | fadeFromLeft()| At the left of image, the percentage of the image that is unchanged is 0%. At the right, every pixel is unchanged|
+| x | maxGreen() | Maximizes the green component of the image|
+| c | maxBlue() | Maximizes the blue component of the image|
+| p | maxPurple() | Maximizes both the red and the blue components of the image|
+| y | maxYellow() | Maximizes both the red and the green components of the image|
+| o | maxCyan() | Maximizes both the blue and the green components of the image| 
+| s | save()    | Saves the image                                              |
+| i | intensify | Intensify                                                    |
